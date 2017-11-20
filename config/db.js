69ec6a635a2006d122a25db1b86e4e4e1ee833b6
Thomas BARRAS <thomas.barras26@gmail.com>
2017-11-20 07:50:14 +0100

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import { prod } from '../config'
+
+const devConfig = {
+    host: process.env.MONGO_HOST || 'localhost',
+    name: process.env.MONGO_NAME || 'NodeGraphQLStarter',
+    port: process.env.MONGO_PORT || 27017,
+}
+
+const prodConfig = {
+    host: process.env.MONGO_HOST || 'localhost',
+    name: process.env.MONGO_NAME || 'NodeGraphQLStarter',
+    port: process.env.MONGO_PORT || 27017,
+}
+
+const config = prod ? prodConfig : devConfig
+export default config
